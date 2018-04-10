2d5b1ad594bc85ed164415a77f727f6e7f6450ad
Moses Gathuku <mosesgathuku95@gmail.com>
2018-04-10 17:00:43 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 document.getElementById('soccerodds').style.display='none';
+document.getElementById('register').style.display='none';
+document.getElementById('logindiv').style.display='none';
+
 document.getElementById('football').addEventListener('click',function(){
     document.getElementById('default').style.display='none';
     document.getElementById('soccerodds').style.display='block';
@@ -7,4 +10,32 @@ document.getElementById('football').addEventListener('click',function(){
 document.getElementById('backbtn').addEventListener('click',function(){
     document.getElementById('default').style.display='block';
     document.getElementById('soccerodds').style.display='none';
+});
+
+document.getElementById('registerbtn').addEventListener('click',function(){
+    document.getElementById('default').style.display='none';
+    document.getElementById('soccerodds').style.display='none';
+    document.getElementById('logindiv').style.display='none';
+    document.getElementById('register').style.display='block';
+});
+
+document.getElementById('loginbtn').addEventListener('click',function(){
+    document.getElementById('default').style.display='none';
+    document.getElementById('soccerodds').style.display='none';
+    document.getElementById('logindiv').style.display='block';
+    document.getElementById('register').style.display='none';
+});
+
+document.getElementById('regcancel').addEventListener('click',function(){
+    document.getElementById('default').style.display='block';
+    document.getElementById('soccerodds').style.display='none';
+    document.getElementById('logindiv').style.display='none';
+    document.getElementById('register').style.display='none';
+});
+
+document.getElementById('logincancel').addEventListener('click',function(){
+    document.getElementById('default').style.display='block';
+    document.getElementById('soccerodds').style.display='none';
+    document.getElementById('logindiv').style.display='none';
+    document.getElementById('register').style.display='none';
 });
